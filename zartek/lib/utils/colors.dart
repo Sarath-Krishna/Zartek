e7866d0dd6354d3efb7d3a8f21c86b5002b1d014
sarathmacbookpro <sarathkrishnan222@gmail.com>
2021-12-21 00:25:28 +0530

    -First Commit -Cart Logic Remaining -Images from API is not accessible to my network

---
+++ zartek/lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color kBlueGoogleSign = Colors.blueAccent;
+  static const Color kTransparent = Colors.transparent;
+  static const Color kWhite = Colors.white;
+  static const Color kLightGreen = Colors.lightGreen;
+  static const Color kGreen = Colors.green;
+}
---
+++ zartek/lib/utils/images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const kFirebaseLogo ='assets/images/firebase_logo.png';
+  static const kGoogleLogo ='assets/images/Google-Logo.png';
+}
